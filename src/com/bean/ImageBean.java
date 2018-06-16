a4b7cf70c06acb5e7107054562ffd2cbfed1833f
w6180358w <272416634@163.com>
2018-06-16 13:39:20 +0800

    。

---
+++ src/com/bean/ImageBean.java
@@ -0,0 +1,40 @@
+package com.bean;
+
+public class ImageBean {
+
+	private Integer height;
+	private Integer width;
+	private String base64;
+	private String path;
+	private String suffix;
+	public Integer getHeight() {
+		return height;
+	}
+	public void setHeight(Integer height) {
+		this.height = height;
+	}
+	public Integer getWidth() {
+		return width;
+	}
+	public void setWidth(Integer width) {
+		this.width = width;
+	}
+	public String getBase64() {
+		return base64;
+	}
+	public void setBase64(String base64) {
+		this.base64 = base64;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getSuffix() {
+		return suffix;
+	}
+	public void setSuffix(String suffix) {
+		this.suffix = suffix;
+	}
+}
